2af3d925dfebbeecc3e85e3e714cbf332919e3b2
sumit mourya <sumitmourya@Sumits-MacBook-Air.local>
2024-03-13 20:58:42 +0530

    csp issue

--- backend/app.js
+++ backend/app.js
@@ -23,23 +23,6 @@ app.use(
 app.use(cors());
 
 app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      scriptSrc: [
-        "'self'",
-        "'unsafe-inline'",
-        "https://www.googletagmanager.com/gtag/js",
-        "https://apis.google.com/js/platform.js",
-        "https://js.stripe.com/v3",
-      ],
-      connectSrc: ["self", "https://www.google-analytics.com"],
-      imgSrc: ["self", "https://res.cloudinary.com", "data:"],
-    },
-  })
-);
-
-app.use(
   express.json({
     limit: "50mb",
   })
